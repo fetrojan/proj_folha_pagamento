ea527a28b91224e67f0c23343d1471fb465bd7b6
Felipe Rossi Trojan <felipetrojan@hotmail.com>
2024-12-09 10:40:31 -0300

    refactor: funcao gerecniarFolhaPagamento refatorada

--- classes/Funcionario.ts
+++ classes/Funcionario.ts
@@ -15,6 +15,15 @@ class Funcionario {
         this.horasTrabalhadas = [];
     }
 
+    exibirInformacoes():void {
+        console.log(`
+            --------------------------
+            ID: ${this.id}
+            Nome: ${this.nome}
+            Cargo: ${this.cargo}
+            Horas Trabalhadas: ${this.horasTrabalhadas}`)
+    }
+
     registrarHoras(numHoras:number):void {
         this.horasTrabalhadas.push(Number(numHoras));
     }
@@ -31,6 +40,27 @@ class Funcionario {
 
     calcularSalarioMensal(): number {
         return this.calcularTotalHoras() * this.taxaHoraria;
+    }
+
+    calcularInss() :number {
+        let salario = this.calcularSalarioMensal();
+        let inss = 0;
+    
+        if (salario > 4000.04) {
+            inss = salario * 14 / 100;
+        } else if (salario > 2666.69) {
+            inss = salario * 12 / 100;
+        } else if (salario > 1412.01) {
+            inss = salario * 9 / 100;
+        } else {
+            inss = salario * 7.5 / 100;
+        }
+    
+        if (inss > 908.85) {
+            inss = 908.85;
+        }
+    
+        return inss;
     }
 }
 
--- index.ts
+++ index.ts
@@ -8,44 +8,19 @@ const listaFuncionarios: Funcionario[] = [];
 function adicionarFuncionario (nome: string, cargo:string, taxaHoraria:number) : void {
     let funcionario = new Funcionario(nome, cargo, taxaHoraria)
     listaFuncionarios.push(funcionario);
+    console.log('Funcionário adicionado com sucesso!');
 }
 
-function calcularInss(funcionario) {
-    let salario = calcularSalarioMensal(funcionario);
-    let inss = 0;
-
-    if (salario > 4000.04) {
-        inss = salario * 14 / 100;
-    } else if (salario > 2666.69) {
-        inss = salario * 12 / 100;
-    } else if (salario > 1412.01) {
-        inss = salario * 9 / 100;
-    } else {
-        inss = salario * 7.5 / 100;
-    }
-
-    if (inss > 908.85) {
-        inss = 908.85;
-    }
-
-    return inss;
-}
-
-function calcularHorasTrabalhadas(funcionario) {
-    let horasTrabalhadas = funcionario.horasTrabalhadas.reduce((acc, curr) => acc + curr, 0);
-    return horasTrabalhadas;
-}
-
-function gerarRelatorioPagamento() {
+function gerarRelatorioPagamento() : void {
     console.log('********RELATÓRIO DE PAGAMENTO********\n');
 
     listaFuncionarios.map((funcionario) => {
         console.log('Nome: ' + funcionario.nome);
         console.log('Cargo: ' + funcionario.cargo);
         console.log('Horas Trabalhadas: ' + funcionario.calcularTotalHoras());
-        console.log('Salário Bruto: R$ ' + calcularSalarioMensal(funcionario));
-        console.log('Valor do INSS: R$ ' + calcularInss(funcionario));
-        console.log('Salário Líquido: R$ ' + (calcularSalarioMensal(funcionario) - calcularInss(funcionario)));
+        console.log('Salário Bruto: R$ ' + funcionario.calcularSalarioMensal());
+        console.log('Valor do INSS: R$ ' + funcionario.calcularInss());
+        console.log('Salário Líquido: R$ ' + (funcionario.calcularSalarioMensal() - funcionario.calcularInss()));
 
         console.log('--------------------------------    ');
     });
@@ -53,13 +28,14 @@ function gerarRelatorioPagamento() {
     console.log('\n***************************************\n');
 }
 
-function gerenciarFolhaPagamento() {
+function gerenciarFolhaPagamento() :void {
     while (true) {
         console.log('********GERENCIAMENTO DE FOLHA DE PAGAMENTO********\n');
         console.log('1 - Adicionar Funcionário');
         console.log('2 - Registrar Horas Trabalhadas');
-        console.log('3 - Gerar Relatório de Pagamento');
-        console.log('4 - Sair');
+        console.log('3 - Exibir lista de funcionarios');
+        console.log('4 - Gerar Relatório de Pagamento');
+        console.log('5 - Sair');
         console.log('\n***************************************\n');
 
         let opcao = Number(prompt('Digite a opção desejada: '));
@@ -87,9 +63,14 @@ function gerenciarFolhaPagamento() {
                 }
                 break;
             case 3:
-                gerarRelatorioPagamento();
+                listaFuncionarios.map(funcionario => {
+                    funcionario.exibirInformacoes();
+                })
                 break;
             case 4:
+                gerarRelatorioPagamento();
+                break;
+            case 5:
                 console.log('Saindo do gerenciamento de folha de pagamento...');
                 return;
             default:
